9f847c55212a4547b58a1735500e99b76b111b73
Matheus-Moura26 <moura.matheus26@gmail.com>
2022-06-13 22:45:09 -0300

    entregue

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import Pessoa from './Components/Developer.jsx';
+import "./App.css";
+import Pessoa from "./Components/Developer.jsx";
 
 function App() {
   const people = [
-  {name: "Gabriel", age: 19, country:"Brasil"},
-  {name: "Anna", age: 30, country:"Itália"},
-  {name: "Hazackerberg", age: 30, country:"Alemanha"},
-  {name: "Zoyo", age: 30, country:"Nova Zelandia"},
-  ]
+    { name: "Gabriel", age: 19, country: "Brasil" },
+    { name: "Anna", age: 30, country: "Itália" },
+    { name: "Hazackerberg", age: 30, country: "Alemanha" },
+    { name: "Zoyo", age: 30, country: "Nova Zelandia" },
+  ];
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-      {people.map( (item, index) => (<Pessoa 
-      
-      key={index}
-      name={item.name}
-      age={item.age}
-      country={item.country}
-      />))}
+    <div className="App">
+      <header className="App-header">
+        {people.map(({ name, age, country }, index) => (
+          <Pessoa key={index} name={name} age={age} country={country} />
+        ))}
       </header>
     </div>
-    );
+  )
 }
 
 export default App;
--- src/Components/Developer.jsx
+++ src/Components/Developer.jsx
@@ -1,12 +1,12 @@
 import './style.css'
 
-function Pessoa(props){
+function Pessoa({name,age,country}){
     
     return(
         <div className='card'>
-            <label className='title'>"Dev: "{props.name}</label>
-            <label>"Idade: "{props.age}</label>
-            <label>"country: "{props.country}</label>
+            <label className='title'>"Dev: "{name}</label>
+            <label>"Idade: "{age}</label>
+            <label>"country: "{country}</label>
 
         </div>
     )
